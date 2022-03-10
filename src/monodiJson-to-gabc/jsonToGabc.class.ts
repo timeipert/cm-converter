94c285d5a9839bcd1cddffa23606ce6fdbe609c2
timeipert <tim.eipert@gmail.com>
2022-03-10 15:13:01 +0100

    Restructuring #1

---
+++ src/monodiJson-to-gabc/jsonToGabc.class.ts
@@ -0,0 +1,132 @@
+export default class JsonToGabcConverter {
+    private _data: any = {};
+    hasHeader = false;
+    positionInAlphabet;
+    existsZeileContainer;
+
+    constructor() {
+        this.positionInAlphabet = (position_input_alphabet: number, octave: number,
+                                   input_alphabet: string, clef_position: number) =>
+            position_input_alphabet + (octave * input_alphabet.length) - ((clef_position - 1) * 2)
+        this.existsZeileContainer = (d: any) => d
+            .map((e: any) => e['kind'] === "ZeileContainer")
+            .filter((e: boolean) => e).length !== 0;
+
+    }
+
+
+    create_header(name: string, gabcCopyright: string = "",
+                  scoreCopyright: string = "",
+                  officePart: string = "",
+                  occasion: string = "",
+                  meter: string = "",
+                  commentary: string = "",
+                  arranger: string = "",
+                  author: string = "",
+                  date: string = "",
+                  manuscript: string = "",
+                  manuscriptReference: string = "",
+                  manuscriptStoragePlace: string = "",
+                  book: string = "",
+                  language: string = "",
+                  transcriber: string = "",
+                  transcriptionDate: string = "",
+                  mode: string = "",
+                  userNotes: string = "",
+                  annotation: string = ""
+    ) {
+        if (this.hasHeader) {
+            console.warn("Warning: Doc already has a header.")
+            return false;
+        }
+        const header = [
+            "name: ", name, "\n",
+            "gabc-copyright: ", gabcCopyright, "\n",
+            "score-copyright: ", scoreCopyright, "\n",
+            "office-part: ", officePart, "\n",
+            "occasion: ", occasion, "\n",
+            "meter: ", meter, "\n",
+            "commentary: ", commentary, "\n",
+            "arranger: ", arranger, "\n",
+            "author: ", author, "\n",
+            "date: ", date, "\n",
+            "manuscript: ", manuscript, "\n",
+            "manuscript-reference: ", manuscriptReference, "\n",
+            "manuscript-storage-place: ", manuscriptStoragePlace, "\n",
+            "book: ", book, "\n",
+            "language: ", language, "\n",
+            "transcriber: ", transcriber, "\n",
+            "transcription-date: ", transcriptionDate, "\n",
+            "mode: ", mode, "\n",
+            "user-notes: ", userNotes, "\n",
+            "annotation: ", annotation, "\n",
+            "\n"
+        ].join("");
+        this._data = [header, this._data].join("");
+        return true;
+    }
+
+    find_other_clef(clef_position: number, position_of_ps: number,
+                    octave: number, monodi_alphabet: string, gregorio_alphabet: string) {
+        const other_clefs = [1, 2, 3, 4].filter((d) => d !== clef_position);
+        const other_positions = other_clefs.map((d) => {
+            return this.positionInAlphabet(position_of_ps, octave, monodi_alphabet, d);
+        });
+
+        const position_ratings = other_positions.map(d => Math.abs(d - (gregorio_alphabet.length / 2)));
+        const best_clef = other_clefs[position_ratings.indexOf(Math.min(...position_ratings))];
+        console.log(best_clef)
+        const position_in_greg_bc = this.positionInAlphabet(position_of_ps, octave, monodi_alphabet, best_clef);
+        console.log(position_in_greg_bc);
+        return {clef_change: true, clef: best_clef, char: gregorio_alphabet[position_in_greg_bc]};
+    }
+
+    // clef position from top line 1 - 4
+    to_gregorio_char(pitch_symbol: string, octave: number, clef_position: number = 1) {
+        const gregorio_alphabet = "abcdefghijklm";
+        const monodi_alphabet = "abcdefg";
+
+
+        const position_of_ps = monodi_alphabet.indexOf(pitch_symbol);
+        const position_in_greg = this.positionInAlphabet(position_of_ps, octave, monodi_alphabet, clef_position);
+        if (position_in_greg < 0 || position_in_greg >= gregorio_alphabet.length) {
+            return this.find_other_clef(clef_position, position_of_ps, octave, monodi_alphabet, gregorio_alphabet);
+        } else {
+            return {clef_change: false, char: gregorio_alphabet[position_in_greg]};
+        }
+    }
+
+    create_syllable() {
+
+    }
+
+    create_note() {
+
+    }
+
+    // @ts-ignore
+    get data() {
+        return this._data
+    }
+
+    flatStaffRecur(d: any): any {
+        if (this.existsZeileContainer(d)) {
+            return d;
+        } else {
+            return d.reduce((p: any, c: any) => {
+                p.push(...this.flatStaffRecur(c['children']));
+                return p;
+            }, [])
+        }
+    }
+
+    getFlatStaffs() {
+        this.flatStaffRecur(this._data['children']);
+    }
+
+    transform(data: string) {
+        this._data = JSON.parse(data);
+    }
+
+
+}
---
+++ src/monodiJson-to-gabc/jsonToGabc.test.ts
@@ -0,0 +1,86 @@
+import GabcDocument from './jsonToGabc.class';
+import {expect, assert} from 'chai';
+import 'mocha';
+
+describe('Gatbc Model', () => {
+    const doc = new GabcDocument();
+    describe('create_header', () => {
+        it('should return a valid header', () => {
+            const hasNoHeader = doc.create_header("1", "2", "3",
+                "4", "5", "6", "7", "8",
+                "9", "10", "11", "12",
+                "13", "14", "15", "16", "17", "18",
+                "19", "20");
+            expect(hasNoHeader).be.true
+            expect(doc.data).to.equal(`name: 1
+gabc-copyright: 2
+score-copyright: 3
+office-part: 4
+occasion: 5
+meter: 6
+commentary: 7
+arranger: 8
+author: 9
+date: 10
+manuscript: 11
+manuscript-reference: 12
+manuscript-storage-place: 13
+book: 14
+language: 15
+transcriber: 16
+transcription-date: 17
+mode: 18
+user-notes: 19
+annotation: 20
+
+`);
+        });
+    })
+    describe('to_gregorio_char', () => {
+        it('should return the right character when clef change is not needed', () => {
+            const char1 = doc.to_gregorio_char("a", 0, 1);
+            expect(char1['char']).to.equal("a");
+            expect(char1['clef_change']).to.be.false;
+            const char2 = doc.to_gregorio_char("d", 1, 1);
+            expect(char2['char']).to.equal("k");
+            expect(char2['clef_change']).to.be.false;
+        });
+        it('should return the right character when clef change is needed', () => {
+            const char3 = doc.to_gregorio_char("g", 1, 1);
+            expect(char3['char']).to.equal("h");
+            expect(char3['clef_change']).to.be.true;
+            const char4 = doc.to_gregorio_char("e", 1, 3);
+            expect(char4['char']).to.equal("h");
+            expect(char4['clef_change']).to.be.false;
+        })
+    });
+    describe('getFlatStaffs', () => {
+        it('should flat with 1 level', () => {
+            const ex = `{
+                "kind": "RootContainer",
+                "uuid": "b9c3268a-e987-472e-9f96-aee2f696b31c",
+                "children": [
+                    {
+                        "uuid": "5c4fd9b3-3299-4e9f-846d-26945e54a9bf",
+                        "kind": "FormteilContainer",
+                        "children": [{
+                            "uuid": "112eef3a-f25a-44ec-a116-00cd75020c22",
+                            "kind": "ZeileContainer",
+                            "children": [
+                                {}
+                            ]
+                        }]
+                    }]
+            };`;
+            doc.transform(ex);
+            const out = JSON.stringify(doc.getFlatStaffs());
+            expect(out).to.equal(`{
+                            "uuid": "112eef3a-f25a-44ec-a116-00cd75020c22",
+                            "kind": "ZeileContainer",
+                            "children": [
+                                {}
+                            ]
+                        }`)
+        })
+    })
+});
